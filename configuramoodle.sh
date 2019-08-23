cfde8a6ac420bd2f9cd1e0eb85a7d9be71537039
redhatbsb <40247249+redhatbsb@users.noreply.github.com>
2019-08-23 08:47:19 -0300

    Create configuramoodle.sh

---
+++ configuramoodle.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+apt-get -y update
+apt-get -y install git
+git clone -b MOODLE_36_STABLE git://git.moodle.org/moodle.git
+cd moodle && mv * /var/www/html/
+mkdir /moodledata
+chmod -R 777 /etc/cron.d/
+chmod -R 777 /var/run/
